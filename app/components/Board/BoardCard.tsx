0dc7ea266c7d46fa873a52957ee8e57cc8930b38
Hwanju <hwanju1596@gmail.com>
2024-02-27 15:05:26 +0900

    :bug: emotion lib 추가 및 tooltip 수정

--- app/components/Board/BoardCard.tsx
+++ app/components/Board/BoardCard.tsx
@@ -1,6 +1,7 @@
+"use client";
 
 import { BoardCardType } from '@/types/board-type';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Tooltip, Typography } from '@mui/material';
 import React, { useState } from 'react'
 
 
@@ -10,14 +11,11 @@ export const BoardCard = ( {title, url, createTime} : BoardCardType) => {
     return (
         <Card >
             <CardContent onMouseEnter={() => setShowMousehover(true)} onMouseLeave={() => setShowMousehover(false)}>
-                <Typography variant="body2">
-                    {title}
-                </Typography>
-                {showMousehover && (
-                    <Typography variant="caption">
-                        {String(createTime)}
+                <Tooltip title={String(createTime)} arrow>
+                    <Typography variant="body2">
+                        {title}
                     </Typography>
-                )}
+                </Tooltip>
             </CardContent>
         </Card>
     );
