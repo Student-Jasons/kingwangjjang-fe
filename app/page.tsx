cd3a28696a387f01ca2b03f465a93ab7e1c50b70
hwanju <hwanju1596@gmail.com>
2024-03-02 03:20:47 +0900

    :arrow_up: next version update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
 import { BestBoard } from "./apollo/board";
 import { BoardCard } from "./components/Board/BoardCard";
+import { BoardList } from "./components/Board/BoardList";
+import { boardData } from "./ testdata/bestboard";
 
 export default function Home() {
   return (
-    <BoardCard
-    site={"dcinside"}
-    title={"Sample Title"}
-    url={"https://example.com"}
-    createTime={new Date()} // 현재 시간으로 createTime을 설정
-  />
+    <div className="flex justify-center items-center h-screen">
+    <BoardList boardList={boardData} />
+  </div>
   );
 }
