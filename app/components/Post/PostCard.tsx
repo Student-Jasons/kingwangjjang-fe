4aa929f43052c99bab4139c2a2a23bf8bb7bfb0b
Hwanju <hwanju1596@gmail.com>
2024-03-13 11:22:09 +0900

    :sparkles: PostList Link 추가

--- app/components/Post/PostCard.tsx
+++ app/components/Post/PostCard.tsx
@@ -1,21 +1,22 @@
-"use client";
-
 import { PostCardType } from '@/types/board-type';
 import { Card, CardContent, Tooltip, Typography } from '@mui/material';
 import React, { useState } from 'react'
+import LaunchIcon from '@mui/icons-material/Launch';
+import Link from 'next/link';
 
-
-export const PostCard = ( {title, url, createTime} : PostCardType) => {
+export const PostCard = ({ title, url, createTime }: PostCardType) => {
     return (
         <Card className='w-screen'>
-            <CardContent>
+            <CardContent className="flex justify-between">
                 <Tooltip title={String(createTime)} arrow>
                     <Typography variant="body2">
                         {title}
                     </Typography>
                 </Tooltip>
+                <Link href={url} target='_blank'>
+                    <LaunchIcon />
+                </Link>
             </CardContent>
         </Card>
     );
 }
-
