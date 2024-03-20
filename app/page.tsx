a8154cdd5a3450f6f74f65fcac541041f9b620fd
Hwanju <hwanju1596@gmail.com>
2024-03-20 10:14:03 +0900

    :art: Zustand 설계

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,21 @@ import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import { PostSummarizer } from "@/components/PostSummarizer";
 import { Container } from "@mui/material";
 import { useGPTStore } from "@/stores/board";
+import { useCallback } from "react";
 
 export default function Home() {
-  const { answer } = useGPTStore();
+  const { answer, setAnswerById } = useGPTStore();
+
+  const handlePostCardClick = (id: String) => {
+    setAnswerById(id, boardData);
+  };
   
   return (
     <Container className="flex max-w-lg items-center h-screen" maxWidth="lg">
         <Grid container className='flex items-center' spacing={2} >
           <Grid xs={12}>
-              <PostList PostList={boardData} />
+              <PostList PostList={boardData} 
+              handlePostCardClick={handlePostCardClick} />
           </Grid>
           <Grid xs={12}>
             <PostSummarizer GPTAnswer={answer}/>
--- app/stores/board.ts
+++ app/stores/board.ts
@@ -1,9 +1,22 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
+import { PostCardType } from '@/types/board-type';
 
 interface GPTStore {
-  answer: string
+  id: number;
+  answer: String;
+  setAnswerById: (id: String, data: PostCardType[]) => void;
 }
 
 export const useGPTStore = create<GPTStore>((set) => ({
-  answer: 'GPT 요약 중..',
-}))
+  id: 0,
+  answer: 'GPT 요약 중...',
+  setAnswerById: (id: String, data: PostCardType[]) => {
+    console.log(data)
+    const post = data.find((item) => item.id === id);
+    if (post) {
+      set({ answer: post.GPTAnswer });
+    } else {
+      set({ answer: '해당 ID에 대한 데이터를 찾을 수 없습니다.' });
+    }
+  },
+}));
