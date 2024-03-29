3538044af999eed6e5a2d439070d4b7584450ac0
Hwanju <hwanju1596@gmail.com>
2024-03-29 14:44:11 +0900

    :art: board/ 생성 (layout, page)

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,30 +1,43 @@
 'use client'
 import { PostList } from "@/components/Post/PostList";
-import { boardData } from "./ testdata/bestboard";
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import { PostSummarizer } from "@/components/PostSummarizer";
 import { Container } from "@mui/material";
 import { useGPTStore } from "@/stores/board";
-import { useCallback } from "react";
+import { gql, useQuery } from '@apollo/client';
+import { boardData } from "../ testdata/bestboard";
+import {InputNameType} from "@/types/board-type"
+const BEST_BOARD = gql`
+  query MyQuery {
+    allRealtime {
+      Id
+      GPTAnswer
+    }
+  }
+`;
 
 export default function Home() {
   const { answer, setAnswerById } = useGPTStore();
+  const { loading, error, data } = useQuery(BEST_BOARD);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  console.log(data);
 
   const handlePostCardClick = (id: String) => {
     setAnswerById(id, boardData);
   };
   
   return (
-    <Container className="flex max-w-lg items-center h-screen" maxWidth="lg">
+      <Container className="flex max-w-lg items-center h-screen" maxWidth="lg">
         <Grid container className='flex items-center' spacing={2} >
           <Grid xs={12}>
-              <PostList PostList={boardData} 
-              handlePostCardClick={handlePostCardClick} />
+            <PostList PostList={boardData} handlePostCardClick={handlePostCardClick} />
           </Grid>
           <Grid xs={12}>
             <PostSummarizer GPTAnswer={answer}/>
           </Grid>
         </Grid>
-    </Container>
+      </Container>
   );
 }
