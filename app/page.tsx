1156ad76d01942d79a440258fd4af3e079c1086e
jde98 <ekrjs004@gmail.com>
2024-04-04 22:54:29 +0900

    :hammer: Container 태그 복구

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header/Header";
 import { ContentWrapper } from "./components/ContentWrapper";
+import { Container } from "@mui/material";
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Header></Header>
       <ContentWrapper />
-    </>
+    </Container>
   );
 }
