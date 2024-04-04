083f6b2e4b097a031ad6d5469a2e8e84aa331968
jde98 <ekrjs004@gmail.com>
2024-04-04 22:48:17 +0900

    :sparkles: useScrollTrigger 훅 사용하여 스크롤 시 헤더 사라지게 동작, 필터 버튼 클릭 시 기존 사이드로 사용하던 Drawer 출력되도록 변경

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,57 +2,65 @@
 
 import {
   AppBar,
+  Avatar,
   Box,
-  Button,
   Drawer,
+  IconButton,
+  Slide,
   Toolbar,
+  Typography,
   useMediaQuery,
+  useScrollTrigger,
   useTheme,
 } from "@mui/material";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import { TemporaryDrawer } from "../Drawer/TemporaryDrawer";
-import { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
+import React, { useState } from "react";
 
 export const Header = () => {
   const [open, setOpen] = useState(false);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-
-  const drawerDirection = isMobile ? "top" : "left";
+  const trigger = useScrollTrigger();
 
   const handleOpen = () => {
     setOpen(true);
   };
 
   return (
-    <Box>
-      <AppBar
-        position="static"
-        color="default"
-        sx={{
-          boxShadow: "none",
-          borderBottom: "1px solid #e0e0e0",
-        }}
-      >
-        <Toolbar>
-          {isMobile && (
-            <Box>
-              <Button onClick={handleOpen} size="large" aria-label="menu">
-                <MenuIcon />
-              </Button>
+    <Box sx={{ height: isMobile ? "54px" : "64px" }}>
+      <Slide appear={false} direction="down" in={!trigger}>
+        <AppBar
+          component="nav"
+          color="default"
+          sx={{
+            display: "flex",
+            boxShadow: "none",
+            borderBottom: "1px solid #e0e0e0",
+          }}
+        >
+          <Toolbar component="div">
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              킹왕짱헤더
+            </Typography>
+            <IconButton onClick={handleOpen}>
+              <FilterAltIcon />
+            </IconButton>
+            <IconButton>
+              <Avatar />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Slide>
 
-              <Drawer
-                open={open}
-                anchor={drawerDirection}
-                onClose={() => setOpen(false)}
-              >
-                <TemporaryDrawer />
-              </Drawer>
-            </Box>
-          )}
-          <div>킹왕짱헤더</div>
-        </Toolbar>
-      </AppBar>
+      <Drawer open={open} onClose={() => setOpen(false)} anchor="right">
+        <TemporaryDrawer />
+      </Drawer>
     </Box>
   );
 };
