93b13b8934164a7061226fee557ea642d3088d81
jde98 <ekrjs004@gmail.com>
2024-04-06 19:39:15 +0900

    :sparkles: 반응형으로 site별로 출력되는 카드 변경(mobileSize: 1, webSize: site.length)

--- app/components/ContentWrapper.tsx
+++ app/components/ContentWrapper.tsx
@@ -1,12 +1,27 @@
-import { Grid } from "@mui/material";
-import { SideBar } from "./Side/SideBar";
-import { PostList } from "./Post/PostList";
-import { boardData } from "../ testdata/bestboard";
+"use client";
+
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import { PostList } from "@/components/Post/PostList";
+import { boardData } from "@/app/ testdata/bestboard";
 
 export const ContentWrapper = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const sites = ["dcinside", "ygosu", "ppomppu"];
+
+  const filteredPosts = sites.map((site) =>
+    boardData.filter((post) => post.site === site)
+  );
+
   return (
     <Grid container sx={{ width: "100%", flexFlow: "row" }}>
-      <PostList PostList={boardData} />
+      <>
+        {isMobile ? (
+          <PostList postItems={boardData} />
+        ) : (
+          filteredPosts.map((posts, index) => <PostList postItems={posts} />)
+        )}
+      </>
     </Grid>
   );
 };
