3527d2e28f18849ac19b5dd91e97f7213811827c
Hwanju <hwanju1596@gmail.com>
2024-04-12 14:41:38 +0900

    :lipstick: Account Menu UI (Avatar)

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,31 +3,38 @@
 import {
   AppBar,
   Avatar,
-  Box,
   Drawer,
   IconButton,
   Link,
   Slide,
   Toolbar,
   Typography,
-  useMediaQuery,
   useScrollTrigger,
-  useTheme,
 } from "@mui/material";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import { TemporaryDrawer } from "@/components/Drawer/TemporaryDrawer";
 import React, { useState } from "react";
+import { AccountMenu } from "./MyPage/AccountMenu";
 
 export const Header = () => {
   const [filterOpen, setFilterOpen] = useState(false);
   const trigger = useScrollTrigger();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
   const handleFilterOpen = () => {
     setFilterOpen(true);
   };
 
+  const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <Box>
+    <>
       <Slide appear={false} direction="down" in={!trigger}>
         <AppBar component="nav" color="default"
           sx={{
@@ -42,18 +49,25 @@ export const Header = () => {
             <IconButton onClick={handleFilterOpen}>
               <FilterAltIcon />
             </IconButton>
-            <IconButton>
-              <Link href="/login">
-                <Avatar />
-              </Link>
+            <IconButton
+                onClick={handleMenuClick}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+              > 
+              <Avatar />
             </IconButton>
           </Toolbar>
         </AppBar>
       </Slide>
 
+      {/* Filter Drawer */}
       <Drawer open={filterOpen} onClose={() => setFilterOpen(false)} anchor="right">
         <TemporaryDrawer />
       </Drawer>
-    </Box>
+
+      {/* Avata Menu */}
+      <AccountMenu anchorEl={anchorEl} open={open} handleClose={handleMenuClose} />
+    </>
   );
 };
