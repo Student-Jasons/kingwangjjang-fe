09b92f48f92a3e330123089bfca91d88d541a5ed
Hwanju <hwanju1596@gmail.com>
2024-04-26 15:48:22 +0900

    :pencil2: PostList Porps

--- app/components/Post/PostList.tsx
+++ app/components/Post/PostList.tsx
@@ -1,9 +1,9 @@
 import { List, ListItem  } from "@mui/material";
 import { PostCard } from "./PostCard";
-import { AllRealtimeQuery } from "@/app/__generated__/graphql";
+import { BoardSummaryType } from "@/app/__generated__/graphql";
 
 interface Props {
-  postItems: AllRealtimeQuery['allRealtime'];
+  postItems: BoardSummaryType[];
   onClickCard: (boardId: string, stie: string) => void;
 }
 
@@ -21,12 +21,12 @@ export const PostList = ({ postItems, onClickCard }: Props) => {
           <ListItem key={index}>
             <PostCard
               onClickToggle={onClickCard}
-              id={post.boardId}
-              site={post.site}
-              title={post.title}
-              url={post.url}
+              id={post.boardId as string}
+              site={post.site as string}
+              title={post.title as string}
+              url={post.url as string}
               createTime={post.createTime}
-              GPTAnswer={post.GPTAnswer}
+              GPTAnswer={post.GPTAnswer as string}
             ></PostCard>
           </ListItem>
         ))}
