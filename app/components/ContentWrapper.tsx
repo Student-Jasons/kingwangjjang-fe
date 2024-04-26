de772a23489737619fa152ae43456fb6a6059e56
Hwanju <hwanju1596@gmail.com>
2024-04-26 15:22:45 +0900

    :art: 페이지네이션을 위한 API 구조 변경

--- app/components/ContentWrapper.tsx
+++ app/components/ContentWrapper.tsx
@@ -2,22 +2,23 @@
 import { useMediaQuery, useTheme } from "@mui/material";
 import { PostList } from "@/components/Post/PostList";
 import { gql } from "@/gql/gql";
-import {  AllRealtimeQuery, SummaryBoardMutation } from "@/gql/graphql";
+import {  BoardContentsByDateDocument, SummaryBoardMutation, MutationSummaryBoardArgs, SummaryBoardDocument } from "@/gql/graphql";
 import { useGPTStore } from "@/stores/board";
-import { useMutation, useQuery } from "@apollo/client";
+import { TypedDocumentNode, useMutation, useQuery } from "@apollo/client";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { useState } from "react";
 
 const REALTIME = gql(`
-query AllRealtime {
-  allRealtime {
-    id
+query BoardContentsByDate($index: String!) {
+  boardContentsByDate(index: $index) {
     boardId
     site
+    rank
     title
     url
     createTime
     GPTAnswer
-  }
+}
 }`);
 
 const SUMMARY_BOARD = gql(`
@@ -32,32 +33,33 @@ export const ContentWrapper = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const sites = ["dcinside", "ygosu"]; // 새로운 값 생성될 때 추가
-  const { setAnswer, reset } = useGPTStore();
-  const { loading: realtimeQueryLoading, error: realtimeQueryError, data: realtimeQueryData } = useQuery<AllRealtimeQuery>(REALTIME);
-  const [ summaryBoardMutation, { data: summaryBoardMutationData, loading: summaryBoardMutationLoading, error: summaryBoardMutationError,},] = useMutation<SummaryBoardMutation>(SUMMARY_BOARD, { refetchQueries: ["AllRealtime"] });
-  const filteredPosts = sites.map( (site) =>  realtimeQueryData?.allRealtime && realtimeQueryData?.allRealtime.filter((post) => post && post.site === site ));
+  const [pageIndex, setPageIndex] = useState<String>();
+
+  const { loading: boardContentsQueryLoading, error: boardContentsQueryError, data: boardContentsData } 
+  = useQuery (BoardContentsByDateDocument, {variables: { index: "0" },});
+  const [ summaryBoardMutation, { data: summaryBoardMutationData, loading: summaryBoardMutationLoading, error: summaryBoardMutationError,},]
+  = useMutation(SummaryBoardDocument, { refetchQueries: ["BoardContentsByDate"] });
   
+  const filteredPosts = sites.map( (site) =>  boardContentsData?.boardContentsByDate && boardContentsData.boardContentsByDate.filter((post) => post && post.site === site ));
   const handleSummaryBoard = (boardId: string, site: string) => {
-    reset();
     summaryBoardMutation({
       variables: { boardId: boardId, site: site },
-      refetchQueries: ['AllRealtime'],
+      refetchQueries: ['BoardContentsByDate'],
       async onQueryUpdated(observableQuery) {
           await observableQuery.refetch();
-          // setAnswer(observableQuery.getCurrentResult().data.allRealtime.find((board: any) => board.Id === boardId).GPTAnswer)
       },
     });
   }
 
-  if (realtimeQueryLoading) return <p>Loading...</p>;
-  if (realtimeQueryError) return <p>Error : {realtimeQueryError.message}</p>;
-  if(isMobile) return realtimeQueryData?.allRealtime && <PostList onClickCard={handleSummaryBoard} postItems={realtimeQueryData.allRealtime} />
+  if (boardContentsQueryLoading) return <p>Loading...</p>;
+  if (boardContentsQueryError) return <p>Error : {boardContentsQueryError.message}</p>;
+  if(isMobile) return boardContentsData?.boardContentsByDate && <PostList onClickCard={handleSummaryBoard} postItems={boardContentsData.boardContentsByDate} />
 
   return (
     <Grid container spacing={2}>
         {
           filteredPosts.map((posts, index) => (
-            realtimeQueryData ? (
+            boardContentsData ? (
               <Grid xs={12} md={6} key={index}>
                 <PostList onClickCard={handleSummaryBoard} postItems={posts} />
               </Grid>
