725368ad1ec4d882c942a25a1e9f29cf0c00fb42
Hwanju <hwanju1596@gmail.com>
2024-05-02 16:04:26 +0900

    :poop: Test Code

--- app/components/Post/PostCard.tsx
+++ app/components/Post/PostCard.tsx
@@ -8,10 +8,11 @@ import {
   Typography,
 } from "@mui/material";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import LaunchIcon from "@mui/icons-material/Launch";
 import Chip from "@/components/UI/Chip";
 import { theme } from "@/app/styles/theme";
+import anime from 'animejs/lib/anime.es.js';
 
 interface Props extends PostCardType {
   onClickToggle: (boardId: string, site: string) => void;
@@ -19,16 +20,44 @@ interface Props extends PostCardType {
 
 export const PostCard = ({ id, site, title, url, createTime, GPTAnswer, rank, onClickToggle, }: Props) => {
   const [expanded, setExpanded] = useState(false);
+  const cardRef = useRef(null); // Ref for the card element
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    // setIsHovering(false);
+  };
+
+  useEffect(() => {
+    const cardElement = null;
+    if (cardElement) {
+      anime({
+        targets: cardElement,
+        // rotate: isHovering ? '-5deg' : '0deg', 
+        autoplay: isHovering ? true : false,
+        translateX: 250,
+        duration: 300,
+        easing: 'easeInOutQuad', 
+      });
+    }
+  }, [isHovering]);
+
   const handleToggle = (boardId: string, site: string) => {
     setExpanded(!expanded);
     if (!expanded) {
       onClickToggle(boardId, site); 
     }
   };
-  const isNotRealtime: boolean = rank !== null
+
+  const isNotRealtime: boolean = rank !== null;
   return (
-    <Card className={'mb-3'} sx={{ width: "100%" }} onClick={ () => handleToggle(id, site) }>
-      <CardContent sx={{ display: "flex", flexDirection: "column" }}>
+    <Card ref={cardRef} className={'mb-3'} sx={{ width: "100%" }} onClick={ () => handleToggle(id, site) } 
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}>
+      <CardContent sx={{transform: 'none', display: "flex", flexDirection: "column" }}>
         <Box>
           <Chip label={site} bgcolor={theme.chip.site} />
           {isNotRealtime && <Chip label={rank} bgcolor={theme.chip.rank}/>}
@@ -48,30 +77,13 @@ export const PostCard = ({ id, site, title, url, createTime, GPTAnswer, rank, on
         </Box>
         <Box>
           <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                padding: "5px 0px",
-              }}
-            >
-              <Typography
-                variant="body2"
-                component="div"
-                sx={{
-                  whiteSpace: "pre-wrap",
-                  wordBreak: "break-word",
-                }}
-              >
+            <Box sx={{ display: "flex", justifyContent: "space-between", 
+                    alignItems: "center",padding: "5px 0px",}}>
+              <Typography variant="body2" component="div"
+                sx={{ whiteSpace: "pre-wrap", wordBreak: "break-word",}}>
                 {GPTAnswer}
               </Typography>
-              <Link
-                href={url}
-                target="_blank"
-                passHref
-                onClick={(e) => e.stopPropagation()}
-              >
+              <Link href={url} target="_blank"passHref onClick={(e) => e.stopPropagation()}>
                 <LaunchIcon />
               </Link>
             </Box>
